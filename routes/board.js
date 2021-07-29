d6c778ceb28616b2c8038f483f209cdf5d640117
Srijon <srijondas@gmail.com>
2021-07-28 21:03:04 -0700

    cosmetic changes

--- routes/board.js
+++ routes/board.js
@@ -484,19 +484,3 @@ router.delete('/boardId/:boardId/classId/:classId/subjectId/:subjectId/chapterId
 
 
 module.exports = router;
-
-//create board
-//add/modify class to board
-//add/modify subject to board, class
-//add/modify chapter to board, class, subject
-//add/modify chapter desc to board, class, subject, chapter
-//add/modify media to board, class, subject, chapter
-//add/modify questions to board, class, subject, chapter
-
-//remove board
-//remove class from board
-//remove subject to board, class
-//remove chapter to board, class, subject
-//remove chapter desc to board, class, subject, chapter
-//remove media to board, class, subject, chapter
-//remove questions to board, class, subject
