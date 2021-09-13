56428213eb3177542651f6aea973edc9387c5aae
Srijon <srijondas@gmail.com>
2021-09-13 11:26:17 -0700

    comment out email sending code

--- config/sendmail.js
+++ config/sendmail.js
@@ -14,7 +14,7 @@ let transporter = nodemailer.createTransport({
     },
   });
 
-  let info = await transporter.sendMail({
+  let info = "email server is not connected during testing, it will be enabled in Production"/*await transporter.sendMail({
     from: 'admin@gitanjali.org', // sender address
     to: emailid, // list of receivers
     subject: subject, // Subject line
@@ -22,10 +22,11 @@ let transporter = nodemailer.createTransport({
     //html: "<b>Hello world?</b>", // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  */
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
 }
