4688cbd3e3db97929fc99e768ef150737164cfa4
aashish321 <mishraaashu123@gmail.com>
2021-09-29 21:11:50 +0530

    change for add chapters in dataloader

--- test/dataloader/CBSE.js
+++ test/dataloader/CBSE.js
@@ -10,7 +10,12 @@ describe ('Starting data loader' , () => {
     describe('Add subjects', function() {
         
         require('./CBSE/class10/sub1socsc/chapter1.js')
+        require('./CBSE/class10/sub1socsc/chapter2.js')
+        require('./CBSE/class10/sub1socsc/chapter3.js')
     })
+
+    
+
 
 
 });
