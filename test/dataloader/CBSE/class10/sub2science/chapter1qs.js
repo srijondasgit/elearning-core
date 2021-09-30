50d6c3916bb0c626344883a609013906e5e93a3a
aashish321 <mishraaashu123@gmail.com>
2021-09-30 08:26:34 +0530

    add chapter

---
+++ test/dataloader/CBSE/class10/sub2science/chapter1qs.js
@@ -0,0 +1,8 @@
+exports.questions = [
+    {"indx":20, 
+    "description":"question 2"},
+    {"indx":30, 
+    "description":"question 3"},
+    {"indx":40, 
+    "description":"question 4"},
+    {" index":50, "description": "question 5"}]
